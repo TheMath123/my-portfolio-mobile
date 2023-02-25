a5000c44f605bdb8934d945ad0fd5ee8b34d7588
Matheus P Agostinho <contact@matheuspa.me>
2023-02-25 16:44:38 -0300

    feat: starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Em breve...</h1>
-      <h2>Comming soon...</h2>
+    <div className="flex flex-col items-center gap-2">
+      <img className="h-20" src="images/logo-m.svg" alt="Logo do Matheus, um M azul" />
+      <h1 className="font-semibold text-xl text-zinc-400">Em breve...</h1>
+      <h2 className="font-semibold text-lg text-zinc-700">Comming soon...</h2>
     </div>
   );
 }
